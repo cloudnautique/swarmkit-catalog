c1b9e8fb7622e6085b1f5f9bd0f00e9776083aaf
Bill Maxwell <cloudnautique@users.noreply.github.com>
2016-08-01 16:41:19 -0700

    updated for v1.12.0 docker (#2)

--- containers/v0.1.0/swarm-mgr/run.sh
+++ containers/v0.1.0/swarm-mgr/run.sh
@@ -3,7 +3,6 @@
 set -x
 
 export META_URL="http://rancher-metadata.rancher.internal/2015-12-19"
-export DOCKER_SWARM_SECRET=$(curl -s ${META_URL}/self/service/uuid)
 /giddyup service wait scale
 
 container_ip()
@@ -12,6 +11,15 @@ container_ip()
     echo ${IP}
 }
 
+is_swarm_manager()
+{
+    active=false
+    if docker -H tcp://${1}:2375 info 2>&1|grep Is\ Manager\:\ true> /dev/null ; then
+        active="true"
+    fi
+
+    echo ${active}
+}
 is_swarm_member()
 {
     active=false
@@ -22,18 +30,19 @@ is_swarm_member()
     echo ${active}
 }
 
-get_manager_agent_ip()
+get_manager_swarm_ip()
 {
-    for container in $(/giddyup service containers -n); do
-        ip=$(container_ip $container)
-        
-        if [ "$(is_swarm_manager ${ip})" = "true"  ]; then
-            UUID=$(curl -s -H 'Accept: application/json' ${META_URL}/self/service/containers/${container}|jq -r '.host_uuid')
-    		IP=$(curl -s -H 'Accept: application/json' ${META_URL}/hosts |jq -r ".[] | select(.uuid==\"${UUID}\") | .agent_ip")
-    		echo ${IP}
-            return 
-        fi
-    done
+    echo $(docker swarm join-token worker | tail -1 | awk '{gsub(/^[ \t]+/,"",$0); split($0,a,":"); print a[1]}')
+    # for container in $(/giddyup service containers -n); do
+        # ip=$(container_ip $container)
+        # 
+        # if [ "$(is_swarm_manager ${ip})" = "true"  ]; then
+            # UUID=$(curl -s -H 'Accept: application/json' ${META_URL}/self/service/containers/${container}|jq -r '.host_uuid')
+    		# IP=$(curl -s -H 'Accept: application/json' ${META_URL}/hosts |jq -r ".[] | select(.uuid==\"${UUID}\") | .agent_ip")
+    		# echo ${IP}
+            # return 
+        # fi
+    # done
 }
 
 get_manager_ip()
@@ -47,6 +56,16 @@ get_manager_ip()
             return 
         fi
     done
+}
+
+manager_secret()
+{
+    echo $(docker -H tcp://$(get_manager_ip):2375 swarm join-token manager|grep token|awk '{print $2}')
+}
+
+worker_secret()
+{
+    echo $(docker -H tcp://$(get_manager_ip):2375 swarm join-token worker|grep token|awk '{print $2}')
 }
 
 demote_node()
@@ -82,32 +101,35 @@ is_swarm_manager()
 
 add_worker()
 {
-    LEADER_IP=$(get_manager_agent_ip)
+    LEADER_DOCKER_IP=$(get_manager_ip)
+    LEADER_IP=$(get_manager_swarm_ip ${LEADER_DOCKER_IP})
     if [ "$(is_swarm_member ${1})" = "true" ] && [ "$(is_swarm_manager ${1})" = "true" ]; then 
             demote_node $(get_swarm_node_id ${1})
     fi
 
     if [ "$(is_swarm_member ${1})" = "false" ]; then
-       docker -H tcp://${1}:2375 swarm join --secret ${DOCKER_SWARM_SECRET} ${LEADER_IP}:2377
+        docker -H tcp://${1}:2375 swarm join --token $(worker_secret) ${LEADER_IP}:2377
     fi
 }
 
 add_manager()
 {
-    LEADER_IP=$(get_manager_agent_ip)
+    LEADER_DOCKER_IP=$(get_manager_ip)
+    LEADER_IP=$(get_manager_swarm_ip ${LEADER_DOCKER_IP})
     if [ "$(is_swarm_member ${1})" = "true" ] && [ "$(is_swarm_manager ${1})" = "false" ]; then 
             promote_node $(get_swarm_node_id ${1})
     fi
 
     if [ "$(is_swarm_member ${1})" = "false" ];then
-        docker -H tcp://${1}:2375 swarm join --manager --secret ${DOCKER_SWARM_SECRET} ${LEADER_IP}:2377
+        docker -H tcp://${1}:2375 swarm join --token $(manager_secret) ${LEADER_IP}:2377
     fi
 }
 
 while true; do
     if /giddyup leader check ; then
         if [ "$(is_swarm_member $(/giddyup leader get))" = "false" ]; then
-            docker swarm init --auto-accept worker --auto-accept manager --secret ${DOCKER_SWARM_SECRET} 
+            # Eth0... hmm... 
+            docker swarm init --advertise-addr eth0
         fi
    
         # reconcile_state
