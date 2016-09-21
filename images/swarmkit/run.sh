7cbf33ddca613d8cd78f19b6b15b35afb04405ad
James Oliver <joliver@rancher.com>
2016-09-21 12:44:33 -0700

    expose relevant configuration via env vars

--- images/swarmkit/run.sh
+++ images/swarmkit/run.sh
@@ -1,3 +1,18 @@
 #!/bin/sh -ex
 
-swarmd -d /tmp/node-1 --listen-control-api /tmp/manager1/swarm.sock --hostname node-1
+SWARM_ELECTION_TICK=${SWARM_ELECTION_TICK:-3}
+SWARM_ENGINE_ADDR=${SWARM_ENGINE_ADDR:-unix:///var/run/docker.sock}
+SWARM_HEARTBEAT_TICK=${SWARM_HEARTBEAT_TICK:-1}
+SWARM_HOSTNAME=${SWARM_HOSTNAME:-$(wget -q -O - http://rancher-metadata.rancher.internal/2015-12-19/self/host/hostname)}
+SWARM_SOCKET=${SWARM_SOCKET:-/var/run/swarm.sock}
+SWARM_DATA_DIR=${SWARM_DATA_DIR:-/etc/swarm}
+SWARM_LOG_LEVEL=${SWARM_LOG_LEVEL:-info}
+
+swarmd \
+  --election-tick $SWARM_ELECTION_TICK \
+  --engine-addr $SWARM_ENGINE_ADDR \
+  --heartbeat-tick $SWARM_HEARTBEAT_TICK \
+  --hostname $SWARM_HOSTNAME \
+  --listen-control-api $SWARM_SOCKET \
+  --log-level $SWARM_LOG_LEVEL \
+  --state-dir $SWARM_DATA_DIR
