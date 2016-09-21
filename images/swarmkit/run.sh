76697dd7dbabb328428282590831c971e52d2d3c
James Oliver <joliver@rancher.com>
2016-09-21 13:05:57 -0700

    flesh out runtime scaling

--- images/swarmkit/run.sh
+++ images/swarmkit/run.sh
@@ -8,11 +8,45 @@ SWARM_SOCKET=${SWARM_SOCKET:-/var/run/swarm.sock}
 SWARM_DATA_DIR=${SWARM_DATA_DIR:-/etc/swarm}
 SWARM_LOG_LEVEL=${SWARM_LOG_LEVEL:-info}
 
-swarmd \
-  --election-tick $SWARM_ELECTION_TICK \
-  --engine-addr $SWARM_ENGINE_ADDR \
-  --heartbeat-tick $SWARM_HEARTBEAT_TICK \
-  --hostname $SWARM_HOSTNAME \
-  --listen-control-api $SWARM_SOCKET \
-  --log-level $SWARM_LOG_LEVEL \
-  --state-dir $SWARM_DATA_DIR
+standalone_node() {
+  # TODO: add host label swarm:manager
+
+  swarmd \
+    --election-tick $SWARM_ELECTION_TICK \
+    --engine-addr $SWARM_ENGINE_ADDR \
+    --heartbeat-tick $SWARM_HEARTBEAT_TICK \
+    --hostname $SWARM_HOSTNAME \
+    --listen-control-api $SWARM_SOCKET \
+    --log-level $SWARM_LOG_LEVEL \
+    --state-dir $SWARM_DATA_DIR
+}
+
+runtime_node() {
+  # TODO: add host label swarm:worker
+
+  swarmd \
+    --election-tick $SWARM_ELECTION_TICK \
+    --engine-addr $SWARM_ENGINE_ADDR \
+    --heartbeat-tick $SWARM_HEARTBEAT_TICK \
+    --hostname $SWARM_HOSTNAME \
+    --listen-control-api $SWARM_SOCKET \
+    --log-level $SWARM_LOG_LEVEL \
+    --state-dir $SWARM_DATA_DIR
+    #--join-addr 127.0.0.1:4242 \
+    #--join-token "<Worker Token>"
+}
+
+rancher_node() {
+  if giddyup leader check; then
+    standalone_node
+  else
+    runtime_node
+  fi
+}
+
+if [ $# -eq 0 ]; then
+    echo No command specified, running in standalone mode.
+    standalone_node
+else
+    eval $1
+fi
